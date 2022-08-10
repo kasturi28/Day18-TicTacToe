1a3fe2896ac82199c391dc1183bcf5544198804b
kasturi28 <kasturipansare2818@gmail.com>
2022-08-10 07:42:48 +0530

    Added_make_choice

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
 
 public class TicTacToe {
 	
 	 public static void main(String[] args) {
 	        System.out.println("Welocome to Tic-tac-Toe Masters");
 	        createArray();
+	        System.out.println("Please enter your choice ,X - O");
+	        makeChoice();
 	    }
 	 
 	    static void createArray(){
@@ -11,6 +14,22 @@ public class TicTacToe {
 	        arr[0] = ' ';
 	        for(int i=1; i<10; i++){
 	            arr[i] = ' ';
+	        }
+	    }
+	    
+	    static void makeChoice(){
+
+	        Scanner sc = new Scanner(System.in);
+	        char user = sc.next().charAt(0);
+	        char computer;
+	        if (user == 'X'){
+	            System.out.println("User choose X");
+	            System.out.println("Computer have O");
+	            computer = 'O';
+	        }else {
+	            System.out.println("User choose O");
+	            System.out.println("Computer have X");
+	            computer = 'X';
 	        }
 	    }
 
